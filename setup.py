9c5c27318d4027eea8f55d9bebfc450a90fcb32f
cocolote <skiel.j.lopez@gmail.com>
2016-06-06 11:32:59 -0400

    modified setup.py so now installs the dependencies by it self

--- setup.py
+++ setup.py
@@ -1,30 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Based on https://github.com/pypa/sampleproject/blob/master/setup.py."""
+from __future__ import unicode_literals
+# To use a consistent encoding
+import codecs
+import os
 from setuptools import setup, find_packages
-from subprocess import call
-from setuptools.command.install import install
-from setuptools.command.bdist_egg import bdist_egg as _bdist_egg
-from dbconn.__init__ import APP_NAME, APP_VERSION
+import sys
 
-class bdist_egg(_bdist_egg):
-    def run(self):
-        call(['pip install -r requirements.txt --no-clean'], shell=True)
-        _bdist_egg.run(self)
+#  Shortcut for building/publishing to Pypi
+# we are not pushing any of this for ATG
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    #  os.system('python setup.py sdist bdist_wheel upload')
+    sys.exit()
+
+
+def parse_reqs(req_path='./requirements.txt'):
+    """Recursively parse requirements from nested pip files."""
+    install_requires = []
+    with codecs.open(req_path, 'r') as handle:
+        # remove comments and empty lines
+        lines = (line.strip() for line in handle
+                 if line.strip() and not line.startswith('#'))
+
+        for line in lines:
+            # check for nested requirements files
+            if line.startswith('-r'):
+                # recursively call this function
+                install_requires += parse_reqs(req_path=line[3:])
+
+            else:
+                # add the line as a new requirement
+                install_requires.append(line)
+
+    return install_requires
+
+
+def parse_readme():
+    """Parse contents of the README."""
+    # Get the long description from the relevant file
+    here = os.path.abspath(os.path.dirname(__file__))
+    readme_path = os.path.join(here, 'README.md')
+    with codecs.open(readme_path, encoding='utf-8') as handle:
+        long_description = handle.read()
+
+    return long_description
+
 
 setup(
-    name=APP_NAME,
-    version=APP_VERSION,
-    description='Connection to the ATG DB with sqlalchemy',
+    name='dbconn',
+
+    # Versions should comply with PEP440. For a discussion on
+    # single-sourcing the version across setup.py and the project code,
+    # see http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.0.1',
+
+    description='Library to connect to a DB with sqlalchemy.',
+    long_description=parse_readme(),
+    # What does your project relate to? Separate with spaces.
+    keywords='dbconn development',
     author='Ezequiel Lopez',
     author_email='elopez@atg.travel',
-    license='PSF',
-    packages=find_packages(),
 
-    install_requires=[
-        'setuptools',
-        'pyodbc>=3.0.10',
-        'python-env>=1.0.0',
-        'PyYAML>=3.11',
-        'SQLAlchemy>=1.0.12',
+    packages=find_packages(exclude=('tests*', 'docs', 'examples')),
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    include_package_data=True,
+    zip_safe=False,
+
+    # Install requirements loaded from ``requirements.txt``
+    install_requires=parse_reqs(),
+
+    # custom dependency links
+    # dependency_links = [
+    #     'http://dev.ts24.com/atg_python_packages/opsconnector-0.0.1.tar.gz',
+    # ],
+
+
+    # test_suite='tests',
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and
+    # allow pip to create the appropriate form of executable for the
+    # target platform.
+    entry_points=dict(
+        console_scripts=[
+            'dbconn = dbconn.__init__:DBconn',
+        ],
+    ),
+
+    # See: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are:
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+
+        'Environment :: Console',
     ],
-
-    cmdclass={ 'bdist_egg': bdist_egg },
-  )
+)
